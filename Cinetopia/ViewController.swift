0368c95adcc7eef9afe7001f9726c6030b453f99
Paulo Andres <110691699+andrvss@users.noreply.github.com>
2024-01-08 15:08:18 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Paulo Andres Gonzales De Oliveira on 08/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
